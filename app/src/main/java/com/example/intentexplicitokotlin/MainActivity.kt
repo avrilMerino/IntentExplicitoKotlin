497fa14ffc1e7e39f965a881f0e0ade665efc252
avrilMerino <merinomorenoavril@gmail.com>
2025-10-27 19:11:23 +0100

    Código casi listo para entregar. Falta aumentar el tamaño de los componentes

--- app/src/main/java/com/example/intentexplicitokotlin/MainActivity.kt
+++ app/src/main/java/com/example/intentexplicitokotlin/MainActivity.kt
@@ -1,26 +1,41 @@
 package com.example.intentexplicitokotlin
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.intentexplicitokotlin.databinding.ActivityMainBinding
+import com.example.intentexplicitokotlin.databinding.ActivityMainBinding //import requerido
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater) //para poder llamar a los elementos
+        setContentView(binding.root) //necesario
 
+        binding.btnEvaluar.setOnClickListener {
+            //si el user escribe algo que no sea número, devuelve null en lugar de dar error
+            val mates  = binding.txtMates.text.toString().trim().toIntOrNull()
+            val fisica = binding.txtFisica.text.toString().trim().toIntOrNull()
+            val quimica= binding.txtQuimica.text.toString().trim().toIntOrNull()
 
+            //Aquí compruebo que las variables son válidas(un poco de seguridad jaja)
+            //creo la lista y compruebo que TODAS no sean NI NULAS y que ESTÉN ENTRE EL 0 Y EL 10
+            val ok = listOf(mates, fisica, quimica).all { it != null && it in 0..10 }
+            //si lo anterior no es verdad, entra en el if siguiente y no hace nada
+            if (!ok) return@setOnClickListener
 
+            //Por último, si los 3 están bien= envía un Intent q es un mensaje que indica a android que abrir
+            startActivity(
+                Intent(this, SegundaActivity::class.java).apply {
 
+                    putExtra("Mates", mates!!) //la exclamacióm se usa para decir "estoy segura de que no es NULL"
+                    putExtra("Fisica", fisica!!)
+                    putExtra("Quimica", quimica!!)
 
-
-
-
-
-
+                }
+            )
+        }
     }
 }
--- app/src/main/java/com/example/intentexplicitokotlin/SegundaActivity.kt
+++ app/src/main/java/com/example/intentexplicitokotlin/SegundaActivity.kt
@@ -1,14 +1,28 @@
 package com.example.intentexplicitokotlin
-
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
+import com.example.intentexplicitokotlin.databinding.ActivitySegundaBinding
+import kotlin.math.roundToInt
 class SegundaActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySegundaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySegundaBinding.inflate(layoutInflater)//convierte el archivo activity_segunda.xml en un objeto Kotlin con acceso directo a sus vistas
+        setContentView(binding.root)
 
+        val mates = intent.getIntExtra("Mates", -1) //recoge el valor enviado con la clave Mates
+        val fisica = intent.getIntExtra("Fisica", -1)
+        val quimica = intent.getIntExtra("Quimica", -1)
+
+        binding.lblNotaMates.text = "Nota de Matemáticas $mates" //Se concatena en kotlin la nota de mates y se pone en el label
+        binding.lblNotaFisica.text = "Nota de Física $fisica"
+        binding.lblNotaQumica.text = "Nota de Química $quimica"
+
+        val media = ((mates + fisica + quimica) / 3.0).roundToInt() //la media de las notas
+
+        val resultado = if (media < 5) "Suspenso" else "Aprobado"
+
+        binding.lblNotaFinal.text = "$resultado con $media de media"
     }
 }
