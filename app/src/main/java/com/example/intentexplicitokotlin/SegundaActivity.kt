6aecbc95b25bfd1eb1d997999e9e586ae054ad88
avrilMerino <merinomorenoavril@gmail.com>
2025-10-27 13:47:46 +0100

    Segundo activity hecho. Falta enlazarlo y la lógica

--- app/src/main/java/com/example/intentexplicitokotlin/SegundaActivity.kt
+++ app/src/main/java/com/example/intentexplicitokotlin/SegundaActivity.kt
@@ -1,26 +1,14 @@
 package com.example.intentexplicitokotlin
+
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.intentexplicitokotlin.databinding.ActivityMainBinding
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
 
+class SegundaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-
-
-
-
-
-
-
-
 
     }
 }
